ea7604a254e78b4b8e7e51ac4729df062fcd7e48
Owen Voke <owen.voke@worksome.com>
2022-09-14 12:06:48 +0100

    feat: make `to` and `channel` unique key

--- database/migrations/create_mfa_multi_factors_table.php
+++ database/migrations/create_mfa_multi_factors_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->boolean('is_valid')->default(false);
 
             $table->timestamps();
+
+            $table->unique(['to', 'channel']);
         });
     }
 
