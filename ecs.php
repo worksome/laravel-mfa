9cf8fa80f35cd8a412ce03e22cb98450d5f201f0
Owen Voke <owen.voke@worksome.com>
2022-10-01 00:36:02 +0100

    chore: add ECS config file

---
+++ ecs.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Worksome\CodingStyle\WorksomeEcsConfig;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/ecs.php',
+        __DIR__ . '/rector.php',
+        __DIR__ . '/tests',
+    ]);
+
+    WorksomeEcsConfig::setup($ecsConfig);
+};
